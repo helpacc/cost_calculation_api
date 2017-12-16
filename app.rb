9c42acc9b512b4ae05fd181e3f8d06fb180e8b13
Anton Chuchkalov <hedgesky@gmail.com>
2017-12-17 01:00:58 +0300

    abort request on missing lat or long params

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra'
 require 'sinatra/json'
+require 'pry-byebug'
 
 get '/v1/cost' do
+  return abort_on_lat_long_missing if lat_or_long_missing?
   json(cost_in_cents: 10_000, currency: 'RUR')
+end
+
+private
+
+def lat_or_long_missing?
+  params['lat'].to_s.empty? || params['long'].to_s.empty?
+end
+
+def abort_on_lat_long_missing
+  status :unprocessable_entity
+  json(message: '"lat" and "long" params should be present')
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -13,10 +13,9 @@ RSpec.describe 'Cost Calculation API' do
     JSON.parse(last_response.body)
   end
 
-  # it would look clumsy if we inline :query param default value
+  # arguably it looks clumsy with :query default value inlined in method definition
   def ask_api_for_cost(query: nil)
-    query ||= {}
-    get('/v1/cost', query)
+    get('/v1/cost', query || { lat: '60', long: '60' })
   end
 
   it 'responds successfully with JSON object' do
@@ -30,5 +29,16 @@ RSpec.describe 'Cost Calculation API' do
     ask_api_for_cost
     expect(json_body['cost_in_cents']).to be_an(Integer)
     expect(json_body['currency']).to be_a(String)
+  end
+
+  it 'requires "lat" and "long" query parameters' do
+    ask_api_for_cost(query: {})
+    expect(last_response.status).to eq 422
+
+    ask_api_for_cost(query: { lat: '60' })
+    expect(last_response.status).to eq 422
+
+    ask_api_for_cost(query: { long: '60' })
+    expect(last_response.status).to eq 422
   end
 end
