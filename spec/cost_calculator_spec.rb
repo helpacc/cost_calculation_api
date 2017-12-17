4ba9597107bb4cf2705fe5103c8243061c0c53c8
Anton Chuchkalov <hedgesky@gmail.com>
2017-12-17 23:09:47 +0300

    readme updates

--- spec/cost_calculator_spec.rb
+++ spec/cost_calculator_spec.rb
@@ -3,16 +3,16 @@ require_relative '../lib/tariff'
 require_relative '../lib/route'
 
 RSpec.describe CostCalculator do
-  let(:example_tariff) {
+  let(:example_tariff) do
     Tariff.new(request_cost: 50, minute_cost: 10, dist_unit_cost: 3,
                minimal_cost: 100)
-  }
+  end
 
   describe '#calculate' do
     it 'calculates cost based on route and tariff' do
       route = Route.new(minutes: 15, distance: 2)
       cost = CostCalculator.new(route, example_tariff).calculate
-      expect(cost.cents).to eq(50 + 10*15 + 3*2)
+      expect(cost.cents).to eq(206) # 50 + 10*15 + 3*2
     end
 
     it "respects tariff's minimal cost" do
